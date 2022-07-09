a4ba259f27fb7d89af5f78be906afe3a341fcb31
cyoon <72480778+yoonchairyeon@users.noreply.github.com>
2022-07-09 16:25:06 +0900

    Create 9498.java

---
+++ 조건문/9498.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+
+		if (90 <= A && A <= 100) {
+			System.out.println("A");
+		}
+		if (80 <= A && A <= 89) {
+			System.out.println("B");
+		}
+		if (70 <= A && A <= 79) {
+			System.out.println("C");
+		}
+		if (60 <= A && A <= 69) {
+			System.out.println("D");
+		}
+		if (A <= 59) {
+			System.out.println("F");
+		}
+
+	}
+}
