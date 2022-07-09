b8faa1e9d088e2bd86d9ab2e62e5a0012e4c768e
cyoon <72480778+yoonchairyeon@users.noreply.github.com>
2022-07-09 16:27:27 +0900

    Create 1546.java

---
+++ 1차원배열/1546.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int a[] = new int[N];
+		for(int i=0; i<N; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		float M=0,avg=0;
+		
+		for(int value : a){
+			if(value > M) {
+				M = value;
+			}
+		}
+		for(int i=0; i<a.length; i++){
+			avg += (a[i]/M*100)/N;
+		}	
+		System.out.println(avg);
+	}
+}
